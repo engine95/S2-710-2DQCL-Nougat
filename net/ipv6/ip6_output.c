2c9d8f051234209488c14e159a0b15a85361184c
Jiri Pirko <jiri@resnulli.us>
2013-11-06 17:52:19 +0100

    ip6_output: fragment outgoing reassembled skb properly
    [ Upstream commit 9037c3579a277f3a23ba476664629fda8c35f7c4 ]
    
    If reassembled packet would fit into outdev MTU, it is not fragmented
    according the original frag size and it is send as single big packet.
    
    The second case is if skb is gso. In that case fragmentation does not happen
    according to the original frag size.
    
    This patch fixes these.
    
    Signed-off-by: Jiri Pirko <jiri@resnulli.us>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Conflicts:
    	net/ipv6/ip6_output.c

--- net/ipv6/ip6_output.c
+++ net/ipv6/ip6_output.c
@@ -150,9 +150,7 @@ static int ip6_finish_output2(struct sk_buff *skb)
 static int ip6_finish_output(struct sk_buff *skb)
 {
 	if ((skb->len > ip6_skb_dst_mtu(skb) && !skb_is_gso(skb)) ||
-	    dst_allfrag(skb_dst(skb)) ||
-		(IP6CB(skb)->frag_max_size && skb->len > IP6CB(skb)->frag_max_size))
-		return ip6_fragment(skb, ip6_finish_output2);
+	    dst_allfrag(skb_dst(skb)))
 	else
 		return ip6_finish_output2(skb);
 }
